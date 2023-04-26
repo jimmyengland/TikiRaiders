3a28870921eb594c028d2da40d56cfe83f5e66ce
jimmyengland <eng15005@byui.edu>
2023-04-25 19:32:13 -0500

    updating blog pages

--- fontend/astro.config.mjs
+++ fontend/astro.config.mjs
@@ -2,12 +2,14 @@
 
 import { defineConfig } from 'astro/config';
 import sanity from "astro-sanity";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
     projectId: '143n24zn',
     dataset: 'tikiraidersdata',
     apiVersion: '2023-04-12',
-    useCdn: false,
+    useCdn: false
   })]
 });
